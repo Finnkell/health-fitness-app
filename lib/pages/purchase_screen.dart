86a51384a4985247adf7bd895bed2d52cb4729c4
Eric Batista <klose.eric31@gmail.com>
2020-03-21 15:01:24 -0300

    Done home_screen

---
+++ lib/pages/purchase_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:health_fitness_app/constants.dart';
+
+class PurchaseScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
